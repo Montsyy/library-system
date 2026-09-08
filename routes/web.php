138dfde917924b152cf237a3f52d827f114d2ed9
Montsyy <dhafifarezi6@gmail.com>
2026-09-08 11:09:25 +0700

    (Praktikum 2) Implement BookController and update routes to use controller for books index

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/books', function() {
-    return view('books.index');
-});
+Route::get('/books', [BookController::class, 'index']);
