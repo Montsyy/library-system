9fef80b8fc119610b2175ef53435a01cb2949d66
Montsyy <dhafifarezi6@gmail.com>
2026-09-18 08:59:06 +0700

    Add layout template and update book index view

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,6 +1,11 @@
-<h1>Daftar Buku</h1>
-<ul>
-    @foreach ($books as $book)
-        <li>{{ $book }}</li>
-    @endforeach
-</ul>
+@extends('layouts.app')
+@section('title', 'Daftar Buku')
+
+@section('content')
+    <h1>Daftar Buku</h1>
+    <ul>
+        @foreach($books as $book)
+            <li>{{ $book }}</li>
+        @endforeach
+    </ul>
+@endsection
